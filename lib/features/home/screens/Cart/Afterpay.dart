3265b1bc327ae2cf05a566bec3b9c7d5e0906144
sirocco39 <christianiwan39@gmail.com>
2025-02-26 13:57:42 +0700

    Cart system progress 1

--- lib/features/home/screens/Cart/Afterpay.dart
+++ lib/features/home/screens/Cart/Afterpay.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pawrentingreborn/common/widgets/appBar/appBar2.dart';
 import 'package:pawrentingreborn/features/home/screens/home.dart';
+import 'package:pawrentingreborn/navigationMenu.dart';
 import 'package:pawrentingreborn/utils/constants/colors.dart';
 
 class AfterPay extends StatefulWidget {
@@ -16,7 +17,8 @@ class _AfterPayState extends State<AfterPay> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: TColors.primary,
-      appBar: const TAppBar2(title: 'After Payment', subtitle: 'Payment in progress'),
+      appBar: const TAppBar2(
+          title: 'After Payment', subtitle: 'Payment in progress'),
       body: Center(
         child: Container(
           width: MediaQuery.of(context).size.width * 0.8, // 80% of screen width
@@ -48,21 +50,25 @@ class _AfterPayState extends State<AfterPay> {
               const SizedBox(height: 20),
 
               /// **Centered Button**
-              Center( 
+              Center(
                 child: ElevatedButton(
                   onPressed: () {
-                    Get.to(()=>Home());
+                    Get.to(() => NavigationMenu());
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: TColors.purplebutton,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
                   ),
                   child: const Text(
                     "Back to Home",
-                    style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w700),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700),
                   ),
                 ),
               ),
